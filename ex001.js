dfbf0817cb634e27544627d9da46dfe8c691cba0
yanohue <yanohue@gmail.com>
2023-02-12 17:45:56 -0300

    add js exercises

---
+++ ex001.js
@@ -0,0 +1,13 @@
+var num = 1
+//prompt()
+
+var resultado
+
+for(var i = 0; i <= 10; i++){
+    calcularTabuada(num ,i)
+    console.log(`${num} x ${i} = ${resultado}`)  
+}
+
+function calcularTabuada(num1, num2){
+    return resultado = num1 * num2;
+}
