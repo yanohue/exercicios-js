3379489d76d733f87361d8961036b37b4643365d
yanohue <108890917+yanohue@users.noreply.github.com>
2023-03-31 20:33:08 -0300

    Created table class

---
+++ to-do/roman_project/table_class.js
@@ -0,0 +1,11 @@
+export default class Table {
+    constructor( M , D , C , L , X , V , I ) {
+        this.M = M
+        this.D = D
+        this.C = C
+        this.L = L
+        this.X = X
+        this.V = V
+        this.I = I
+    }
+}
